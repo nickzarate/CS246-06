f02b61b744c56d64c1216f90fa5fd134b5cbefd4
Nick <nicholas.zarate@gmail.com>
2016-06-01 11:55:24 -0600

    Add progress bar, list view, and buttons

--- app/src/main/java/com/example/nickzarate/cs246_06/MainActivity.java
+++ app/src/main/java/com/example/nickzarate/cs246_06/MainActivity.java
@@ -1,7 +1,13 @@
 package com.example.nickzarate.cs246_06;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStreamReader;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,40 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    // Called when "Create" button is clicked
+    public void create() {
+        String filename = "numbers.txt";
+        FileOutputStream outputStream;
+
+        try {
+            outputStream = openFileOutput(filename, Context.MODE_PRIVATE);
+            for (int i = 1; i < 11; i++) {
+                String string = Integer.toString(i) + "\n";
+                outputStream.write(string.getBytes());
+                Thread.sleep(250);
+            }
+            outputStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Called when "Load" button is clicked
+    public void load() {
+        Context.getFilesDir("numbers.txt", 0);
+        FileInputStream fis = Context.openFileInput("hello.txt", Context.MODE_PRIVATE);
+        InputStreamReader isr = new InputStreamReader(fis);
+        BufferedReader bufferedReader = new BufferedReader(isr);
+        StringBuilder sb = new StringBuilder();
+        String line;
+        while ((line = bufferedReader.readLine()) != null) {
+            sb.append(line);
+        }
+    }
+
+    // Called when "Clear" button is clicked
+    public void clear() {
     }
 }
